15e9c94b57a0732d4425d59622cd93834af9d49c
rayquaza7 <sarhaangulati737@gmail.com>
2022-07-05 23:52:58 -0700

    add events and storage slots

--- script/Dev.s.sol
+++ script/Dev.s.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
-contract Dev is Script {}
+contract Dev is Script {
+    function run() public returns (bytes32) {
+        return keccak256(abi.encodePacked("URI(string,uint256)"));
+    }
+}
